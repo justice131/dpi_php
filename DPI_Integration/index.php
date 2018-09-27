8827500a9f7cc1912b073163266e0cc5ccaa3784
Dav <Dav@138.25.151.165>
2018-09-27 14:08:55 +1000

    change E

--- DPI_Integration/index.php
+++ DPI_Integration/index.php
@@ -114,13 +114,13 @@ and open the template in the editor.
                                                 <input type="checkbox" id="Approvals-CAT-Manning" onclick="show_gis_Manning_approvals('Approvals-CAT-Manning')"> <font size="2">Work approvals </font>
                                         </div> 
                                      
-                                        <div id="link_to_parallel_coordinate" class="link_to_parallel">
+<!--                                        <div id="link_to_parallel_coordinate" class="link_to_parallel">
                                             <a href="parallel_coordinate_macquarie_main.php" target="_blank">Insight</a>
                                         </div>
                                     
                                         <div id="link_to_parallel_coordinate_manning" class="link_to_parallel">
                                             <a href="parallel_coordinate_manning_main.php" target="_blank">Insight</a>
-                                        </div>
+                                        </div>-->
                                         <div id="container"></div>
                                 </div>
 			</div>
@@ -733,14 +733,14 @@ and open the template in the editor.
 //                var checkBox = document.getElementById(id); 
                 var geojsonfile = MacquarieBogan_unregulated;
                 var geojsonfile_1 = Macquarie_Unregulatedriver;
-                link_to_parr = document.getElementById('link_to_parallel_coordinate');
+//                link_to_parr = document.getElementById('link_to_parallel_coordinate');
                 var elem = document.createElement("div");
                 elem.setAttribute('id', 'unreg_mac');
                 elem.innerHTML = ('<img src="lib/leaflet/images/U.png"  width="17" height="18.2" align = "center">&nbsp; &nbsp;Unregulated river<br>');
 //                if (checkBox.checked === true){
                     document.getElementById("legend").appendChild(elem);
                     // display link icon
-                    link_to_parr.style.display = 'block';                                       
+//                    link_to_parr.style.display = 'block';                                       
                                     
                     if (typeof controlSearch !== 'undefined') {
                         map.removeControl(controlSearch);
@@ -2768,9 +2768,9 @@ and open the template in the editor.
 //                var checkBox = document.getElementById(id); 
                 var geojsonfile = Manning_unregulated;
                 var geojsonfile_1 = Manning_Unregulatedriver;
-                link_to_parr_man = document.getElementById('link_to_parallel_coordinate_manning');
+//                link_to_parr_man = document.getElementById('link_to_parallel_coordinate_manning');
 //                if (checkBox.checked === true){
-                    link_to_parr_man.style.display='block';
+//                    link_to_parr_man.style.display='block';
                     document.getElementById("legend").appendChild(elem);
                     if (typeof controlSearch !== 'undefined') {
                         map.removeControl(controlSearch);
